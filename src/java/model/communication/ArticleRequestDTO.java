b6c0dd29515fdaf4384460bf3126cea18a461032
nix19gremory <118509826+nix19gremory@users.noreply.github.com>
2023-11-26 20:09:47 +0100

    new

--- src/java/model/communication/ArticleRequestDTO.java
+++ src/java/model/communication/ArticleRequestDTO.java
@@ -4,24 +4,15 @@
  */
 package model.communication;
 
-import generalisation.annotations.DBField;
-import generalisation.annotations.DBTable;
-
 /**
  *
- * 
  * @author To Mamiarilaza
  */
-@DBTable(name = "article_request_dto", sequenceName = "seq_article_request_dto")
 public class ArticleRequestDTO {
     /// Field
-    @DBField(name = "id_proforma_request_supplier", isForeignKey = true)
     int idProformaRequestSupplier;
-    @DBField(name = "id_article")
     int idArticle;  // ID Article du client
-    @DBField(name = "article_name")
     String articleName;
-    @DBField(name = "quantity")
     double quantity;
     
     /// Getter and setter
@@ -53,23 +44,13 @@ public class ArticleRequestDTO {
     public double getQuantity() {
         return quantity;
     }
+
     public void setQuantity(double quantity) {
         this.quantity = quantity;
     }
-    public void setQuantity(String quantity) throws Exception {
-        if(quantity.trim().equals("") || quantity == null) {
-            throw new Exception("La quantite ne doit pas etre null");
-        }
-        double quantityParsed;
-        try {
-            quantityParsed = Double.valueOf(quantity);
-        } catch(Exception e) {
-            throw new Exception("La quantite ne doit pas etre null");
-        }
-        this.setQuantity(quantityParsed);
-    }
     
     /// Constructor
+
     public ArticleRequestDTO(int idProformaRequestSupplier, int idArticle, String articleName, double quantity) {
         this.idProformaRequestSupplier = idProformaRequestSupplier;
         this.idArticle = idArticle;
@@ -77,12 +58,4 @@ public class ArticleRequestDTO {
         this.quantity = quantity;
     }
     
-    public ArticleRequestDTO(String articleName, String quantity) throws Exception {
-        try {
-            this.setArticleName("Milk");
-            this.setQuantity(quantity);
-        } catch(Exception e) {
-            throw e;
-        }
-    }
 }
--- src/java/model/communication/SupplierInfo.java
+++ src/java/model/communication/SupplierInfo.java
@@ -4,31 +4,24 @@
  */
 package model.communication;
 
-import generalisation.annotations.DBField;
-import generalisation.annotations.DBTable;
-
 /**
  *
  * @author To Mamiarilaza
  */
-@DBTable(name = "supplier_info", sequenceName = "seq_supplier_info")
 public class SupplierInfo {
     // Field
-    @DBField(name = "id_supplier_info", isPrimaryKey = true)
     int idSupplier;
-    @DBField(name = "supplier_name")
     String supplierName;
-    @DBField(name = "supplier_address")
     String supplierAddress;
-    @DBField(name = "responsable_contact")
     String responsableContact;
-    @DBField(name = "mail")
     String mail;
     
-/// Getter and setter
+    // Getter and setter
+
     public int getIdSupplier() {
         return idSupplier;
     }
+
     public void setIdSupplier(int idSupplier) {
         this.idSupplier = idSupplier;
     }
@@ -36,6 +29,7 @@ public class SupplierInfo {
     public String getSupplierName() {
         return supplierName;
     }
+
     public void setSupplierName(String supplierName) {
         this.supplierName = supplierName;
     }
@@ -43,6 +37,7 @@ public class SupplierInfo {
     public String getSupplierAddress() {
         return supplierAddress;
     }
+
     public void setSupplierAddress(String supplierAddress) {
         this.supplierAddress = supplierAddress;
     }
@@ -50,6 +45,7 @@ public class SupplierInfo {
     public String getResponsableContact() {
         return responsableContact;
     }
+
     public void setResponsableContact(String responsableContact) {
         this.responsableContact = responsableContact;
     }
@@ -57,16 +53,13 @@ public class SupplierInfo {
     public String getMail() {
         return mail;
     }
+
     public void setMail(String mail) {
         this.mail = mail;
     }
     
-/// Constructor
+    // Constructor
 
-    public SupplierInfo() {
-    }
-
-    
     public SupplierInfo(int idSupplier, String supplierName, String supplierAddress, String responsableContact, String mail) {
         this.idSupplier = idSupplier;
         this.supplierName = supplierName;
@@ -74,14 +67,5 @@ public class SupplierInfo {
         this.responsableContact = responsableContact;
         this.mail = mail;
     }
-
-    public SupplierInfo(String supplierName, String supplierAddress, String responsableContact, String mail) {
-        this.supplierName = supplierName;
-        this.supplierAddress = supplierAddress;
-        this.responsableContact = responsableContact;
-        this.mail = mail;
-    }
-    
-///Fonctions
     
 }
